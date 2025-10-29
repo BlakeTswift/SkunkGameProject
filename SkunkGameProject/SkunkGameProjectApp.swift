d3c4846c212a715bbf7db16a40b18d46bd4d3651
BlakeT <BlakeTswift@gmail.com>
2025-10-29 14:28:58 -0600

    Initial Commit

---
+++ SkunkGameProject/SkunkGameProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkunkGameProjectApp.swift
+//  SkunkGameProject
+//
+//  Created by Trytten, Blake - Student on 10/29/25.
+//
+
+import SwiftUI
+
+@main
+struct SkunkGameProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SkunkGameProjectTests/SkunkGameProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  SkunkGameProjectTests.swift
+//  SkunkGameProjectTests
+//
+//  Created by Trytten, Blake - Student on 10/29/25.
+//
+
+import Testing
+@testable import SkunkGameProject
+
+struct SkunkGameProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
